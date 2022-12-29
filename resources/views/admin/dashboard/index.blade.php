173a4a73b8fdc7b4926208a6e0065738e708ffef
AXZV <unggulsurya97@gmail.com>
2022-12-29 16:56:52 +0700

    revisi 28/12

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -539,19 +539,19 @@
                             }
                         },
                         series: [{
-                            name  : 'Input Solicit',
+                            name  : 'Total Input',
                             data  : @json($dtsolicit),
                             color : '#ebb501',
                         },{
-                            name  : 'Solicit App',
+                            name  : 'Approve Solicit',
                             data  : @json($dtsolicitapp),
                             color : '#ff5733',
                         },{
-                            name  : 'Prospek App',
+                            name  : 'Approve Prospek',
                             data  : @json($dtprospect),
                             color : '#c70039',
                         },{
-                            name  : 'Data Close',
+                            name  : 'Data Close/Approve Pipeline',
                             data  : @json($dtclose),
                             color : '#900c3e',
                         },{
@@ -647,6 +647,7 @@
                         paging: true,
                         bFilter: true,
                         bInfo: false,
+                        aaSorting: [],
                         dom: 'rtip',
                         pageLength : 3,
                         lengthMenu: [[3, 6, 9, -1], [3, 6, 9, 'Todos']],
